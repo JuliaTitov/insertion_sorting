54ed630a4e3c0c4cb8bbc41640edd42b1e85e8d0
Julia <titova.ula91@gmail.com>
2020-09-03 00:02:34 +0300

    add solution

---
+++ src/Lesson51_sortirovkaVstavkami.java
@@ -0,0 +1,39 @@
+/*Сортировка вставками - алгоритм сортировки, в котором элементы вхоной последовательности просматриваются по одному,
+ *  и каждый новый поступивший элемент размещается в подходящее место среди ранее упорядоченных элементов*/
+public class Lesson51_sortirovkaVstavkami {
+
+	public static void main(String[] args) {
+		int [] array = new int [10];
+		fill(array);
+		print(array);
+		sort(array);
+		print(array);
+	}
+	static void fill(int [] array) {
+		for (int i = 0; i < array.length; i++) {
+			array[i] = (int)(Math.random() * 100);
+		}
+	}
+	static void print(int [] array) {
+		for (int i = 0; i < array.length; i++)
+			System.out.println("array[" + i + "]" + "=" + array[i]);
+		System.out.println();
+	}
+	static void sort(int [] array) {
+		for(int i = 1; i < array.length; i++) {
+			if(array[i] < array[i-1]) {
+				int vstavka = array[i];
+				int index = i;
+				while ((index > 0) && (vstavka < array[index-1])) {
+					array[index] = array[index-1];
+					index--;		
+				}
+				array[index] = vstavka;
+			}
+		}
+	}
+				
+}
+	
+
+
